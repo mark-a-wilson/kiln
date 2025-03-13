d97bea5d4b7eab257d31b887496329165caa03d8
David Okulski <32730627+DavidOkulski@users.noreply.github.com>
2025-03-13 09:24:39 -0700

    Switch to keycloak check sso

--- src/keycloak.tsx
+++ src/keycloak.tsx
@@ -30,8 +30,7 @@ export const initializeKeycloak = async (): Promise<KeycloakInstance | void> => 
         const initOptions: KeycloakInitOptions = {
             pkceMethod: 'S256',
             checkLoginIframe: false,
-            onLoad: 'check-sso',
-            silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`
+            onLoad: 'check-sso'
         };
 
         const auth: boolean = await _kc.init(initOptions);
